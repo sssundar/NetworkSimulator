6925ad5ec4cf8e7fbc00ca4d35545bca2c1d1273
Sith Domrongkitchaiporn <sdomrong@caltech.edu>
2015-10-21 10:39:00 -0700

    high level class

---
+++ Event.cpp
@@ -0,0 +1,9 @@
+#include "Event.hpp"
+
+public void set_completion_time(double time) {
+    completion_time = time;
+}
+
+public double get_completion_time() {
+    return completion_time;
+}
---
+++ Reporter.hpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+class Reporter {
+
+public:
+    void get_id();
+
+private:
+    string ID;
+};
