badebbca7ea524ab980920a2f64b93d486605ffb
Sith <sdomrong@caltech.edu>
2015-11-03 20:55:09 -0800

    Added comments

--- Code/Python/parser.py
+++ Code/Python/parser.py
@@ -1,18 +1,27 @@
-"""
-Parser File
-@ Sith Domrongkitchaiporn
+# Parser Code
+# Sith Domrongkitchaiporn
 
-"""
-
-#%%
 import json
-from test import Test as test
+from host import host
+from link import link
+from router import router
+from flow import flow
+
+"""
+Parser Function
+
+Input:      .json file  (Example = input.json)
+return:     hash table(nodes)
+
+Nodes can be flow, link, router or host. Check input.json for the format.
+
+"""
 
 def parser (myfile):
     with open(myfile) as file:    
         data = json.load(file)
     
-    map = {}    
+    map = {}    # Create Hash Table   
     
     for line in data:
         print line["type"]
@@ -35,17 +44,12 @@ def parser (myfile):
         elif line["type"] == "flow":
             print "F"
             f = flow(line["id"], line["source"], line["dest"], line["size"],line["start"])
-            map[line["id"]] = f            
+            map[line["id"]] = f 
+            
+            # Set flow for the hosts
+            map[line["source"]].set_flow(line["id"])
+            map[line["dest"]].set_flow(line["id"])    
 
-        """   
-        h = test()
-        h.append(2)
-        h.append('m')
-        h.append("hi sith")
-        print h.getval()
-        if in map            
-        """
-    
     return map    
 
 myfile = 'input.json'
